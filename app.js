31847095e9275dcd84bafda820e7a69da5ef2c32
RJ Nel <114648273+RJ3128@users.noreply.github.com>
2024-10-22 19:18:10 +0200

    Create connection

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
 
 // Middleware (if needed)
 app.use(express.json());
 
+const mongoURI = 'mongodb://cmsadmin:49ysEC9F7Uw@178.128.198.86:27017/cms_stock_database';
+
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error('MongoDB connection error:', err));
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
