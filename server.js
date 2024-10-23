2a9538ed79300eaad57a4a447d9e77da936bb597
RJ Nel <114648273+RJ3128@users.noreply.github.com>
2024-10-23 17:40:24 +0200

    Create base api and can connect to mongo

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
+
 const app = express();
 const port = 3000;
-
-// Middleware (if needed)
-app.use(express.json());
 
 const mongoURI = 'mongodb://cmsadmin:49ysEC9F7Uw@178.128.198.86:27017/cms_stock_database';
 
@@ -16,7 +14,6 @@ app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
-
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server is running on port ${port}`);
 });
